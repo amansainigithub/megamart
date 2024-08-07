024cd9d1206adbe2dc3e0c3971102e7185be4629
aman <amansaini1407@gmail.com>
2024-08-08 01:23:20 +0530

    AES Algo

--- spring-boot-server/src/main/java/com/coder/springjwt/controllers/customer/customerAuthController/CustomerAuthController.java
+++ spring-boot-server/src/main/java/com/coder/springjwt/controllers/customer/customerAuthController/CustomerAuthController.java
@@ -9,6 +9,7 @@ import com.coder.springjwt.payload.customerPayloads.freshUserPayload.VerifyMobil
 import com.coder.springjwt.services.customerServices.customerAuthService.CustomerAuthService;
 import com.coder.springjwt.util.MessageResponse;
 import com.coder.springjwt.util.ResponseGenerator;
+import com.google.gson.Gson;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
@@ -19,7 +20,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
 import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(CustomerUrlMappings.CUSTOMER_BASE_URL)
@@ -69,6 +76,52 @@ public class CustomerAuthController {
     public ResponseEntity<?> customerForgotPasswordFinal(@Valid @RequestBody CustForgotPasswordPayload custForgotPasswordPayload) {
         return this.customerAuthService.customerForgotPasswordFinal(custForgotPasswordPayload);
     }
+
+
+    private static final String SECRET_KEY = "1234567890008iu7yhygtfredfvgbhgg"; // Use the same key as in Angular
+    private final Gson gson = new Gson();
+    private final SecretKeySpec secretKeySpec = getKeySpec(SECRET_KEY);
+    @PostMapping("/sswordFinal")
+    public ResponseEntity<?> sswordFinal(@RequestBody Map<String, String> encryptedData) throws Exception {
+        System.out.println("STARTING DATA 1111 " );
+        String encryptedJson = encryptedData.get("data");
+        System.out.println("encrypted Data :: " + encryptedJson);
+        String decryptedJson = decrypt(encryptedJson, secretKeySpec);
+        Map<String, Object> data = gson.fromJson(decryptedJson, Map.class);
+
+        // Do something with the data
+        System.out.println("Decrypted JSON data: " + data);
+
+        // Return the decrypted data as a response (for demonstration purposes)
+        return ResponseEntity.ok(data);
+    }
+
+    public static SecretKey generateKey() throws Exception {
+        KeyGenerator keyGen = KeyGenerator.getInstance(ALGORITHM);
+        keyGen.init(256);
+        return keyGen.generateKey();
+    }
+
+    public static String encrypt(String data, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encryptedData = cipher.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedData);
+    }
+
+    public static String decrypt(String encryptedData, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] decryptedData = cipher.doFinal(Base64.getDecoder().decode(encryptedData));
+        return new String(decryptedData);
+    }
+
+
+    private static final String ALGORITHM = "AES";
+    public static SecretKeySpec getKeySpec(String key) {
+        return new SecretKeySpec(key.getBytes(), ALGORITHM);
+    }
+
 
 
 
